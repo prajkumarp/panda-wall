322d389b90675b080b067ff5a4685113fcae79c8
Panneerselvam, Rajkumar <rajkumar.panneerselvam@jpmchase.com>
2014-06-05 01:25:20 -0400

    slider implementation

---
+++ App/panda-wall/panda-wall/ViewControllers/RKPMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  RKPMenuViewController.h
+//  panda-wall
+//
+//  Created by Panneerselvam, Rajkumar on 6/5/14.
+//  Copyright (c) 2014 Panneerselvam, Rajkumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RKPMenuViewController : UITableViewController
+- (IBAction)unwindToMenuViewController:(UIStoryboardSegue *)segue;
+@end
---
+++ App/panda-wall/panda-wall/ViewControllers/RKPMenuViewController.m
@@ -0,0 +1,19 @@
+//
+//  RKPMenuViewController.m
+//  panda-wall
+//
+//  Created by Panneerselvam, Rajkumar on 6/5/14.
+//  Copyright (c) 2014 Panneerselvam, Rajkumar. All rights reserved.
+//
+
+#import "RKPMenuViewController.h"
+
+@interface RKPMenuViewController ()
+
+@end
+
+@implementation RKPMenuViewController
+
+- (IBAction)unwindToMenuViewController:(UIStoryboardSegue *)segue{}
+
+@end
